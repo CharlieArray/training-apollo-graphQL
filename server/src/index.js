dd3dd9e5fc217694e167fb68ffc92e352c21ad82
Charles <charles@getbackd.com>
2022-02-27 18:30:12 +0200

    init commit

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-//TODO
+const {ApolloServer} = require('apollo-server')
+const typeDefs = require('./schema')
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(()=>{
+    console.log(`
+        Server is running!
+        Listening on port 4000
+        Query at https://studio.apollographql.com/dev    
+    `)
+})
